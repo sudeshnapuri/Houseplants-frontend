feb96e96bfbe761318f985048d76fb7709857b76
sudeshnapuri <sudeshnap@hotmail.co.uk>
2021-12-14 23:33:49 +0000

    HPS-7 #comment added axios getByID to get plants by ID

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const getOutput = document.querySelector("#getOutput");
+const getIdOutput = document.querySelector("#getIdOutput");
 
+// GET ALL HOUSEPLANTS
 const getPlants = () => {
     axios
     .get("http://localhost:8081/getAll")
@@ -58,6 +60,8 @@ const getPlants = () => {
 }
 getPlants();
 
+
+// CREATE/POST HOUSEPLANTS
 document.querySelector("#houseplantForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
@@ -82,6 +86,7 @@ document.querySelector("#houseplantForm").addEventListener("submit", function(ev
         .catch(err => console.error(err));
     });
 
+// DELETE HOUSEPLANT BY ID
     document.querySelector("#deleteForm").addEventListener("submit", function(event) {
         event.preventDefault();
     
@@ -98,3 +103,63 @@ document.querySelector("#houseplantForm").addEventListener("submit", function(ev
             })
             .catch(err => console.error(err));
     });
+// GET HOUSEPLANT BY ID
+        document.querySelector("#houseplantIdForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const form = this;
+        const plantIdFind = form.plantIdFind.value;
+        
+            axios
+            .get(`http://localhost:8081/get/${plantIdFind}`)
+            .then(response => {
+                form.reset();
+                form.plantIdFind.focus();
+                const houseplant = response.data;
+                getIdOutput.innerHTML = "";
+
+                const houseplantIdCol = document.createElement("div");
+                houseplantIdCol.classList.add("col");
+
+                const houseplantIdCard = document.createElement("div");
+                houseplantIdCard.classList.add("card");
+
+                const houseplantIdBody = document.createElement("div");
+                houseplantIdBody.classList.add("card-body");
+
+                const houseplantId = document.createElement("h5");
+                houseplantId.classList.add("card-title");
+                houseplantId.innerText = `ID: ${houseplant.id}`;
+                houseplantIdBody.appendChild(houseplantId);
+
+                const houseplantName = document.createElement("p");
+                houseplantName.classList.add("card-text");
+                houseplantName.innerText = `Plant Name: ${houseplant.plantName}`;
+                houseplantIdBody.appendChild(houseplantName);
+
+                const houseplantWater = document.createElement("p");
+                houseplantWater.classList.add("card-text");
+                houseplantWater.innerText = `Water: ${houseplant.water}`;
+                houseplantIdBody.appendChild(houseplantWater);
+
+                const houseplantSunlight = document.createElement("p");
+                houseplantSunlight.classList.add("card-text");
+                houseplantSunlight.innerText = `Sunlight: ${houseplant.sunlight}`;
+                houseplantIdBody.appendChild(houseplantSunlight);
+
+                const houseplantHumidity = document.createElement("p");
+                houseplantHumidity.classList.add("card-text");
+                houseplantHumidity.innerText = `Humidity: ${houseplant.humidity}`;
+                houseplantIdBody.appendChild(houseplantHumidity);
+
+                const houseplantQuantity = document.createElement("p");
+                houseplantQuantity.classList.add("card-text");
+                houseplantQuantity.innerText = `Quantity: ${houseplant.quantity}`;
+                houseplantIdBody.appendChild(houseplantQuantity);
+
+                houseplantIdCard.appendChild(houseplantIdBody);
+                houseplantIdCol.appendChild(houseplantIdCard);
+
+                getIdOutput.appendChild(houseplantIdCol);
+            })
+            .catch(err => console.error(err));
+            })
