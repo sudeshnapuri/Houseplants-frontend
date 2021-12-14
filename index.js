8fe6970f34070439abe5a65f6ac97c1ecb500ce9
sudeshnapuri <sudeshnap@hotmail.co.uk>
2021-12-14 17:13:00 +0000

    HPS-4 #comment added axios get to view all plants on front-end

--- index.js
+++ index.js
@@ -6,4 +6,54 @@ const getPlants = () => {
     axios
     .get("http://localhost:8081/getAll")
     .then(response => {
+        const houseplants= response.data;
+        getOutput.innerHTML = "";
+        for (let houseplant of houseplants) {
+            const houseplantCol = document.createElement("div");
+            houseplantCol.classList.add("col");
+
+            const houseplantCard = document.createElement("div");
+            houseplantCard.classList.add("card");
+
+            const houseplantBody = document.createElement("div");
+            houseplantBody.classList.add("card-body");
+
+            const houseplantId = document.createElement("h5");
+            houseplantId.classList.add("card-title");
+            houseplantId.innerText = `ID: ${houseplant.id}`;
+            houseplantBody.appendChild(houseplantId);
+
+            const houseplantName = document.createElement("p");
+            houseplantName.classList.add("card-text");
+            houseplantName.innerText = `Plant Name: ${houseplant.plantName}`;
+            houseplantBody.appendChild(houseplantName);
+
+            const houseplantWater = document.createElement("p");
+            houseplantWater.classList.add("card-text");
+            houseplantWater.innerText = `Water: ${houseplant.water}`;
+            houseplantBody.appendChild(houseplantWater);
+
+            const houseplantSunlight = document.createElement("p");
+            houseplantSunlight.classList.add("card-text");
+            houseplantSunlight.innerText = `Sunlight: ${houseplant.sunlight}`;
+            houseplantBody.appendChild(houseplantSunlight);
+
+            const houseplantHumidity = document.createElement("p");
+            houseplantHumidity.classList.add("card-text");
+            houseplantHumidity.innerText = `Humidity: ${houseplant.humidity}`;
+            houseplantBody.appendChild(houseplantHumidity);
+
+            const houseplantQuantity = document.createElement("p");
+            houseplantQuantity.classList.add("card-text");
+            houseplantQuantity.innerText = `Quantity: ${houseplant.quantity}`;
+            houseplantBody.appendChild(houseplantQuantity);
+
+            houseplantCard.appendChild(houseplantBody);
+            houseplantCol.appendChild(houseplantCard);
+
+            getOutput.appendChild(houseplantCol);
+        }
+    })
+    .catch(err => console.error(err));
 }
+getPlants();
